1bd8c9e7838eb1b4aac4b95fe9b57582f5e330c6
shivam <shivam96ti@gmail.com>
2024-05-14 18:50:30 +0530

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'KYXpHStlKKQiHZvokOOtROgunqqbRJPh',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT':'46092',
+    }
 }
 
 
